efca765795bbcba4cdec8f74b370f5b13844af74
ghani-othman <ghani_othman@reazonmy.com>
2024-06-07 16:41:17 +0800

    Initial commit for Dx Backend Test

---
+++ src/main/java/com/dx/dx_be_test/DxBeTestApplication.java
@@ -0,0 +1,13 @@
+package com.dx.dx_be_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxBeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxBeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dx/dx_be_test/DxBeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dx.dx_be_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxBeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
